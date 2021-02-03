8eacc4c85db2747bfe563274b45785238ab4d400
EC2 Default User <ec2-user@ip-172-31-42-234.us-east-2.compute.internal>
2021-02-03 22:15:07 +0000

    seems to work

--- predictor.py
+++ predictor.py
@@ -4,7 +4,8 @@ import sys
 from app.entities.model.prediction import Prediction
 from app.library.model_repository.loader.driver.filesystem import Filesystem
 from app.library.model_repository.repository import Repository
-
+from app.entities.model.model_info import ModelInfo
+import pandas as pd
 
 
 class PythonPredictor:
@@ -13,9 +14,22 @@ class PythonPredictor:
         filename = os.path.join(dirname, 'model.pkl')
         self.model = pickle.load(open(filename, "rb"))
         
+        model_info = ModelInfo(
+            "BRAND_GENDER",
+            "CBCF",
+            "1.0",
+            "2021-01-25-00-17-18"
+        )
+        #åself.model = loader.load_model(model_info)
+
     def predict(self, payload):
+        
         data = payload["user_data"]
-        raw_predictions = self.model.predict(data)
+        if data is not None:
+            user_data = pd.DataFrame(data)
+            raw_predictions = self.model.predict(user_data)
+        else:
+            user_data = pd.DataFrame()
         
         if raw_predictions is None or len(raw_predictions.brand.keys()) < 1:
             return {
@@ -24,14 +38,16 @@ class PythonPredictor:
                 'version': self.model.to_model_info().version,
                 'timestamp': self.model.to_model_info().timestamp,
             }
-
-        predictions = list(map(
-            lambda k: Prediction(
-                brand_id=raw_predictions.brand[k],
-                gender=raw_predictions.gender[k],
-                score=raw_predictions.score[k],
-                liked=raw_predictions.liked[k]
-            ), raw_predictions.brand.keys()))
-
-        return predictions
+        result = {
+            'predictions': []
+        }
+        for x in range(len(raw_predictions.brand.keys())):
+            item = {
+                'brand_id': raw_predictions.brand[x],
+                'gender': raw_predictions.gender[x],
+                'score': raw_predictions.score[x],
+                'liked': raw_predictions.liked[x]
+            }
+            result['predictions'].push(item)
+        return result
 
