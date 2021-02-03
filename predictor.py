c458acd0d73347cf478a6597773ffa7628821b24
EC2 Default User <ec2-user@ip-172-31-42-234.us-east-2.compute.internal>
2021-02-03 20:17:41 +0000

    fixed logic, still no results

--- predictor.py
+++ predictor.py
@@ -8,7 +8,21 @@ class PythonPredictor:
         dirname = os.path.dirname(__file__)
         filename = os.path.join(dirname, 'model.pkl')
         self.model = pickle.load(open(filename, "rb"))
-        
+
     def predict(self, payload):
         data = payload["user_data"]
-        return self.model.predict(data)
+        raw_predictions = self.model.predict(data)
+        
+        if raw_predictions is None or len(raw_predictions.brand.keys()) < 1:
+            return []
+
+        predictions = list(map(
+            lambda k: Prediction(
+                brand_id=raw_predictions.brand[k],
+                gender=raw_predictions.gender[k],
+                score=raw_predictions.score[k],
+                liked=raw_predictions.liked[k]
+            ), raw_predictions.brand.keys()))
+
+        return predictions
+
