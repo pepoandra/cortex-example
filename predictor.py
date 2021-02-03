6fdcfb3073ea979826b9d7bddb555f5945f7caf7
EC2 Default User <ec2-user@ip-172-31-42-234.us-east-2.compute.internal>
2021-02-03 21:00:34 +0000

    yee

--- predictor.py
+++ predictor.py
@@ -1,6 +1,10 @@
 import os
 import pickle
 import sys
+from app.entities.model.prediction import Prediction
+from app.library.model_repository.loader.driver.filesystem import Filesystem
+from app.library.model_repository.repository import Repository
+
 
 
 class PythonPredictor:
@@ -8,13 +12,18 @@ class PythonPredictor:
         dirname = os.path.dirname(__file__)
         filename = os.path.join(dirname, 'model.pkl')
         self.model = pickle.load(open(filename, "rb"))
-
+        
     def predict(self, payload):
         data = payload["user_data"]
         raw_predictions = self.model.predict(data)
         
         if raw_predictions is None or len(raw_predictions.brand.keys()) < 1:
-            return []
+            return {
+                'usecase': self.model.to_model_info().usecase,
+                'model': self.model.to_model_info().model,
+                'version': self.model.to_model_info().version,
+                'timestamp': self.model.to_model_info().timestamp,
+            }
 
         predictions = list(map(
             lambda k: Prediction(
